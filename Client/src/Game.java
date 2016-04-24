9042b394a038c32af665b928870f661a2748e1d0
suvarna4 <suvarna_adit@yahoo.com>
2016-04-24 14:38:37 -0400

    Worked on front-end some more, now can execute

--- Client/src/Game.java
+++ Client/src/Game.java
@@ -1,4 +1,66 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Game {
-
+	Player character;
+	Scanner scan;
+	char input;
+	Player[] enemies;
+	Scenario checkpoint;
+	Scenario current;
+	ArrayList<Character> act;
+	public Game(){
+		scan = new Scanner(System.in);
+		System.out.println("Please enter the name of your character.");
+		String name = scan.next();
+		character = new Player(name);
+		Scenario checkpoint = new Scenario(0,1);
+		current = checkpoint;
+		main();
+	}
+	
+	public void main(){
+		while(true){
+		act = getActions();
+		System.out.println("Your choices are: " + stringActions());
+		String c = scan.next();
+		handleActions(c);
+		}
+	}
+	
+	public ArrayList<Character> getActions(){
+		ArrayList<Character> actions = new ArrayList<Character>();
+		actions.addAll(current.getActions());
+		return actions;
+	}
+	
+	public String stringActions(){
+		StringBuilder sb = new StringBuilder();
+		for(Character c: act){
+			sb.append(c);
+			sb.append(' ');
+		}
+		return sb.toString();
+	}
+	
+	public void handleActions(String c){
+		switch(c){
+		case "f":
+			if(current.forwardScen!=null) current= current.forwardScen;
+			else current = new Scenario(0,1,'f',current);
+			break;
+		case "b":
+			if(current.backScen!=null) current= current.backScen;
+			else current = new Scenario(0,1,'b',current);
+			break;
+		case "l":
+			if(current.leftScen!=null) current= current.leftScen;
+			else current = new Scenario(0,1,'l',current);
+			break;
+		case "r":
+			if(current.rightScen!=null) current= current.rightScen;
+			else current = new Scenario(0,1,'r',current);
+			break;
+		}
+	}
+	
 }
---
+++ Client/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to MushvsGrump");
+		System.out.println("Would you like to start a new game? (y/n)");
+		String next = scan.next();
+		if(next.equals("y")){
+			Game g = new Game();
+		}
+		else System.out.println("Thank you for playing");
+		scan.close();
+	}
+}
